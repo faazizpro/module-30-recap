0d9a50c84ac9866388b7d06eea70cd423d787731
faazizpro <fahadafs11@gmail.com>
2022-08-29 20:13:48 +0600

    Learned How to call a button, add event listener & add na new div

--- es-6-recap/js/dom.js
+++ es-6-recap/js/dom.js
@@ -1,7 +1,40 @@
 document.getElementById('apply-bg').addEventListener('click', function(){
     document.body.style.backgroundColor = '#B1E1FF';
-    const colorDiv = document.getElementsByClassName('friends');
+    const colorDiv = document.getElementsByClassName('friend');
     for(const singleDiv of colorDiv){
         singleDiv.style.backgroundColor = '#9ED2C6';
     }
 })
+
+document.getElementById('apply-LightTeal').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#8FE3CF';
+    const colorDiv2 = document.getElementsByClassName('friend');
+    colorDiv2.style.backgroundColor = '#F5E8C7';
+})
+
+document.getElementById('apply-Periwinkle-Blue').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#9C9EFE';
+    const colorDiv3 = document.getElementsByClassName('friend');
+    colorDiv3.style.backgroundColor = '#FF87B2';
+})
+
+
+document.getElementById('center-third').addEventListener('click', function(){
+    const thirdFriend = document.getElementById('third-friend');
+    thirdFriend.style.textAlign = 'center';
+})
+
+document.getElementById('left-third').addEventListener('click', function(){
+    const thirdFriend = document.getElementById('third-friend');
+    thirdFriend.style.textAlign = 'left';
+})
+document.getElementById('add-friend').addEventListener('click', function(){
+    const friendsContainer = document.getElementById('friends');
+    const friend = document.createElement('div');
+    friend.innerHTML = `
+    <h3 class= 'friend-name'>New Friend</h3>
+    <p>Something New Is Added</p>
+    `
+    friendsContainer.appendChild(friend);
+})
+
