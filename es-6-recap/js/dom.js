8df9738f9dbad1d3f0d7ff4af7cc76fafadcbd2d
faazizpro <fahadafs11@gmail.com>
2022-08-29 16:46:29 +0600

    applied background color on the body and div

--- es-6-recap/js/dom.js
+++ es-6-recap/js/dom.js
@@ -1,3 +1,7 @@
 document.getElementById('apply-bg').addEventListener('click', function(){
     document.body.style.backgroundColor = '#B1E1FF';
+    const colorDiv = document.getElementsByClassName('friends');
+    for(const singleDiv of colorDiv){
+        singleDiv.style.backgroundColor = '#9ED2C6';
+    }
 })
