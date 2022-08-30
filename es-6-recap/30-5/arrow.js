731ba6c27f3c2d5c32458240cdfe9f4d021cd557
faazizpro <fahadafs11@gmail.com>
2022-08-30 19:56:15 +0600

    Completed Arrow function, multiple parameter, function body

---
+++ es-6-recap/30-5/arrow.js
@@ -0,0 +1,14 @@
+//Normal Function
+//This is called function declaration
+function add(first, second) {
+    const total = first + second;
+    return total;
+}
+
+const result = add(10, 33);
+// console.log(result);
+
+//Arrow Function⬇️⬇️⬇️⬇️⬇️⬇️
+const add2 = (first, second) => first + second;
+const result2 = add2(23, 41);
+console.log(result2);
